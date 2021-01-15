f329d0415f9bd13940863188435f9c844b4ab3e8
fgiraud <hello@florent.dev>
2021-01-14 20:32:52 -0500

    chore: add tests + refactorisation to make it clearer + update storyblok js client version

---
+++ cypress/integration/injection.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+describe('Storyblok', () => {
+    it('Storyblok script should have been injected', () => {
+      cy.visit('/')
+      cy.wait(1000)
+
+      cy.window().then((win) => {
+        chai.expect(win).haveOwnProperty('storyblok')
+        chai.expect(win.$nuxt.$storyapi).not.undefined
+        chai.expect(win.$nuxt.$storybridge).not.undefined
+      })
+
+  
+      cy.get('html').then((el) => {
+        const html = el[0].innerHTML;
+        
+        chai
+          .expect(html)
+          .contain('<script src="https://app.storyblok.com/f/storyblok-latest.js" id="storyblok-javascript-bridge"></script>');
+
+        chai
+          .expect(html)
+          .contain('Giraud Florent');
+      })
+    })
+  })
---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import myModule from './'
+
+export default {
+  rootDir: resolve(__dirname, '.'),
+  buildDir: resolve(__dirname, './example/.nuxt'),
+  srcDir: resolve(__dirname, './example'),
+  buildModules: [{ handler: myModule }],
+  storyblok: {
+    accessToken: process.env.STORYBLOK_TOKEN
+  },
+  modules: ['@nuxtjs/axios']
+}
