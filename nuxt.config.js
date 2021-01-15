cd542081131da46b9d62aacf2d48122427bbcef5
fgiraud <hello@florent.dev>
2021-01-14 20:55:55 -0500

    feat: adding support of public runtime configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,13 @@ export default {
   buildDir: resolve(__dirname, './example/.nuxt'),
   srcDir: resolve(__dirname, './example'),
   buildModules: [{ handler: myModule }],
-  storyblok: {
-    accessToken: process.env.STORYBLOK_TOKEN
+  // storyblok: {
+  //   accessToken: process.env.STORYBLOK_TOKEN
+  // },
+  publicRuntimeConfig: {
+    storyblok: {
+      accessToken: process.env.STORYBLOK_TOKEN
+    }
   },
   modules: ['@nuxtjs/axios']
 }
