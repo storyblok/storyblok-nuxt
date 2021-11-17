93ee308bbed5b5a890e84127003966908e9db3d4
Alex Jover <alexjovermorales@gmail.com>
2021-11-17 17:23:01 +0100

    fix(playground): fix playground v-editable

--- playground/app.vue
+++ playground/app.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <h1>Example stories</h1>
-    <div v-editable="blok" data-test="stories">
-      <p v-for="story in stories" :key="story.id">{{ story.name }}</p>
+    <div v-editable="blok.content" data-test="stories">
+      <p v-for="story in state.stories" :key="story.id">{{ story.name }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
+const app = useNuxtApp();
 const storyapi = useStoryApi();
-const { data } = await storyapi.get("cdn/stories");
-const stories = data.stories;
+const { data } = await storyapi.get("cdn/stories", { version: "draft" });
+const state = reactive({ stories: data.stories });
+
+const blok = {
+  content: {
+    _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
+  },
+};
 
 // onMounted(() => {
-//   useStoryBridge(id, (story) => (data.story = story));
+//   useStoryBridge(id, (story) => (state.story = story));
 // });
 </script>
