c2e0d1d097e8d508cdc6733648acb5209f21b1fd
Alvaro <alvaro.saburido@gmail.com>
2022-02-21 19:53:56 +0100

    fix(e2e): removed second call to api on playground

--- playground/app.vue
+++ playground/app.vue
@@ -4,40 +4,33 @@
     <div v-editable="blok.content" data-test="stories">
       <p v-for="story in state.stories" :key="story.id">{{ story.name }}</p>
     </div>
-    <component
-      :is="state.story.content.component"
-      :key="state.story.content._uid"
-      :blok="state.story.content"
-    />
   </div>
 </template>
 
 <script setup>
-const storyapi = useStoryApi();
-const stories = await storyapi.get("cdn/stories", { version: "draft" });
-const demo = await storyapi.get("cdn/stories/demo", { version: "draft" });
+const storyapi = useStoryApi()
+const { data } = await storyapi.get("cdn/stories", { version: "draft" })
 const state = reactive({
-  stories: stories.data.stories,
-  story: demo.data.story,
-});
+  stories: data.stories,
+})
 
 const blok = {
   content: {
     _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
   },
-};
+}
 
 onMounted(async () => {
   useStoryBridge(state.story.id, (event) => {
-    state.story = event;
-  });
-});
+    state.story = event
+  })
+})
 </script>
 
 <script>
 export default {
   mounted() {
-    console.log(this.$storyapi);
+    console.log(this.$storyapi)
   },
-};
+}
 </script>
