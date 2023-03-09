5da5e4d1880bcee526a7f58f8663ebf9d54083d2
alvarosabu <alvaro.saburido@gmail.com>
2023-03-09 10:59:04 +0100

    chore: update deps and migrate cypress

---
+++ lib/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    baseUrl: "http://localhost:3000",
+    viewportWidth: 1600,
+    viewportHeight: 1400
+  },
+});
