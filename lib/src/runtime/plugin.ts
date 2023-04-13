89149e6b982320f5a5b7f05252af216bb2072fcf
Adam Killander <34036252+Adamkillander96@users.noreply.github.com>
2023-04-13 11:01:14 +0200

    fix(): useRuntimeConfig should use the .public property
    Nuxt v3.4 introduced a warning for using public environmental variables without the .public property

--- lib/src/runtime/plugin.ts
+++ lib/src/runtime/plugin.ts
@@ -2,7 +2,7 @@ import { StoryblokVue, apiPlugin } from "@storyblok/vue";
 import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 
 export default defineNuxtPlugin(({ vueApp }) => {
-  let { storyblok } = useRuntimeConfig();
+  let { storyblok } = useRuntimeConfig().public;
   storyblok = JSON.parse(JSON.stringify(storyblok));
   vueApp.use(StoryblokVue, { ...storyblok, use: [apiPlugin] });
 });
