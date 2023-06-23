1e8e02662a1339a1ae0aeb46cfc14d9bf732ae29
Dawntraoz <albasilventefu@gmail.com>
2023-06-23 15:45:18 +0200

    chore: add e2e static version

--- playground/pages/error.vue
+++ playground/pages/error.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import type { ISbStoryData } from "@storyblok/vue";
+/* import type { ISbStoryData } from "@storyblok/vue";
 let story = useState<ISbStoryData>();
 
 try {
   story = await useAsyncStoryblok("vue-test", { version: "draft" });
 } catch (error) {
   console.error(error);
-}
+} */
 </script>
 
 <template>
