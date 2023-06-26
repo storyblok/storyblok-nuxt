5fdf249cd0ec2b37699d957a26350eed8875126b
Dawntraoz <albasilventefu@gmail.com>
2023-06-26 13:30:55 +0200

    chore: add StoryblokComponent to playground

--- playground/pages/[...slug].vue
+++ playground/pages/[...slug].vue
@@ -1,23 +1,24 @@
 <script setup>
 const { slug } = useRoute().params;
-const story = ref();
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join("/") : "home",
+  {
+    version: "draft",
+    language: "en",
+    resolve_relations: ["popular-articles.articles"]
+  }
+);
 
-try {
-  story.value = await useAsyncStoryblok(
-    slug && slug.length > 0 ? slug.join("/") : "home",
-    {
-      version: "draft",
-      language: "en",
-      resolve_relations: ["popular-articles.articles"]
-    }
-  );
-} catch (error) {
-  throw createError(error);
+if (story.value.status) {
+  throw createError({
+    statusCode: story.value.status,
+    statusMessage: story.value.response
+  });
 }
 </script>
 
 <template>
   <div>
-    {{ story }}
+    <StoryblokComponent v-if="story" :blok="story.content" />
   </div>
 </template>
