7a39809ef4ef9504c4dd215a838d5a329e0b602b
alvarosabu <alvaro.saburido@gmail.com>
2023-06-28 10:10:23 +0200

    chore: remove lib

--- lib/src/runtime/plugin.ts
+++
@@ -1,8 +0,0 @@
-import { StoryblokVue, apiPlugin } from "@storyblok/vue";
-import { defineNuxtPlugin, useRuntimeConfig } from "#app";
-
-export default defineNuxtPlugin(({ vueApp }) => {
-  let { storyblok } = useRuntimeConfig().public;
-  storyblok = JSON.parse(JSON.stringify(storyblok));
-  vueApp.use(StoryblokVue, { ...storyblok, use: [apiPlugin] });
-});
