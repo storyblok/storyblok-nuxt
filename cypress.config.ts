6bffa1909ef30985dcf8a420077ac5117cf82c51
alvarosabu <alvaro.saburido@gmail.com>
2023-07-24 10:25:49 +0200

    chore: fix e2e ci start-server

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    baseUrl: "http://localhost:3002",
+    baseUrl: "http://localhost:3000",
     viewportWidth: 1600,
     viewportHeight: 1400
   }
