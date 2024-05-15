82b32849b93b9047f056b2299cced6eeecdef93e
Juho Rutila <juho.rutila@gmail.com>
2024-05-15 10:01:06 +0300

    feat: allow asyncOptions
    print deprecation warning when used the current way

--- src/runtime/composables/useAsyncStoryblok.ts
+++ src/runtime/composables/useAsyncStoryblok.ts
@@ -1,11 +1,14 @@
 import { useStoryblokApi, useStoryblokBridge } from "@storyblok/vue";
 import type { ISbStoriesParams, StoryblokBridgeConfigV2, ISbStoryData } from '@storyblok/vue';
-import { useState, onMounted, useAsyncData } from "#imports";
+import { useState, onMounted, useAsyncData, useRuntimeConfig } from "#imports";
+import { type AsyncDataOptions } from "#imports";
+import deprecate from "util-deprecate";
 
 export const useAsyncStoryblok = async (
   url: string,
   apiOptions: ISbStoriesParams = {},
-  bridgeOptions: StoryblokBridgeConfigV2 = {}
+  bridgeOptions: StoryblokBridgeConfigV2 = {},
+  asyncOptions: AsyncDataOptions = undefined
 ) => {
   const storyblokApiInstance = useStoryblokApi();
   const uniqueKey = `${JSON.stringify(apiOptions)}${url}`;
@@ -21,16 +24,31 @@ export const useAsyncStoryblok = async (
     }
   });
 
-  if (!story.value) {
-    const { data } = await useAsyncData(uniqueKey, () => {
-      return storyblokApiInstance.get(
-        `cdn/stories/${url}`,
-        apiOptions
-      );
-    })
-    if(data) {
-      story.value = data.value?.data.story
+  const options = useRuntimeConfig().public.storyblok;
+  if (!options.asyncDataOutput && asyncOptions === undefined) {
+    if (!story.value) {
+      deprecate(() => {}, "`useAsyncStoryblok` will return an `AsyncData` object ({ data, pending, ... }) in the future. You can change this behavior now by passing an AsyncDataOptions object as the last argument or setting `asyncDataOutput: true` in module options in `nuxt.config`.", "STORYBLOK-DEPR-ASYNC")();
+      const { data } = await useAsyncData(uniqueKey, () => {
+        return storyblokApiInstance.get(
+          `cdn/stories/${url}`,
+          apiOptions
+        );
+      })
+      if(data) {
+        story.value = data.value?.data.story
+      }
     }
+
+    return story
   }
-  return story
+
+  return await useAsyncData(
+    uniqueKey,
+    () => {
+      return storyblokApiInstance
+        .get(`cdn/stories/${url}`, apiOptions)
+        .then((r) => r.data.story)
+    },
+    asyncOptions
+  )
 };
