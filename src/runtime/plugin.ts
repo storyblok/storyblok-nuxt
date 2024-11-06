fe91b4888f9cc29e32babd46c22753c6abf528d5
alvarosabu <alvaro.saburido@gmail.com>
2024-11-06 11:18:54 +0100

    fix: fake change to force new version to be able to use pkg.pr.new

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,6 +1,8 @@
 import { apiPlugin, StoryblokVue } from '@storyblok/vue';
 import { defineNuxtPlugin, useRuntimeConfig } from '#app';
 
+// TODO: This comment is used to generate a new version with semantic release, remove it on next version
+
 export default defineNuxtPlugin(({ vueApp }) => {
   let { storyblok } = useRuntimeConfig().public;
   storyblok = JSON.parse(JSON.stringify(storyblok));
